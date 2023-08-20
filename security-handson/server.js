f02ba8bc3782d36e7823f37b5d7d1cfd46277011
taiki-yamaguchi <tykid16@gmail.com>
2023-08-21 00:57:29 +0900

    :sparkles: add static file

--- security-handson/server.js
+++ security-handson/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));  
+
 app.get("/", (req, res, next) => {
     res.end("Top Page");
 });
